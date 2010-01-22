b322d43c6ff9f8f88eb10fa270df844931050a28
Steven Walter <stevenrwalter@gmail.com>
2010-01-22 07:16:47 -0500

    No more error 2, but data transfer still gives error

--- obex.c
+++ obex.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <glib.h>
 
+static int req_done=0;
+
 volatile int loop = 0;
 
 typedef struct {
@@ -37,6 +39,10 @@ static void obex_requestdone (obex_state *state, obex_t *hdl,
             }
             break;
 
+        case OBEX_CMD_DISCONNECT:
+            printf("cmd disconnect\n");
+            break;
+
         case OBEX_CMD_PUT:
             printf("cmd put\n");
             while (OBEX_ObjectGetNextHeader(hdl, obj, &header_id, 
@@ -64,6 +70,8 @@ static void obex_requestdone (obex_state *state, obex_t *hdl,
         default:
             printf("Funny command\n");
     }
+
+    req_done++;
 }
 
 static void obex_event (obex_t *hdl, obex_object_t *obj, int mode,
@@ -75,21 +83,23 @@ static void obex_event (obex_t *hdl, obex_object_t *obj, int mode,
     obex_headerdata_t hd;
     int size;
 
-    printf("event!\n");
-
     state = OBEX_GetUserData(hdl);
 
+    if (event == OBEX_EV_PROGRESS) {
+        hd.bq4 = 0;
+        size = 4;
+        OBEX_ObjectAddHeader(hdl, obj, OBEX_HDR_CONNECTION, hd, size, OBEX_FL_FIT_ONE_PACKET);
+        return;
+    }
+
+    printf("event!\n");
     if (obex_rsp != OBEX_RSP_SUCCESS && obex_rsp != OBEX_RSP_CONTINUE) {
-        printf("Got questionable response %x\n", obex_rsp);
+        printf("FAIL %x\n", obex_rsp);
+        req_done++;
+        return;
     }
     
     switch (event) {
-        case OBEX_EV_PROGRESS:
-            hd.bq4 = 0;
-            size = 4;
-            OBEX_ObjectAddHeader(hdl, obj, OBEX_HDR_CONNECTION, hd, size, OBEX_FL_FIT_ONE_PACKET);
-            break;
-
         case OBEX_EV_REQDONE:
             obex_requestdone(state, hdl, obj, obex_cmd, obex_rsp);
             break;
@@ -134,6 +144,8 @@ int syncml_obex_send_req () {
         i = OBEX_InterfaceConnect(handle, obex_intf);
         printf("connect = %d\n", i);
 
+        OBEX_SetTransportMTU(handle, 0x400, 0x400);
+
         obj = OBEX_ObjectNew(handle, OBEX_CMD_CONNECT);
         hd.bs = "LivescribeService";
         size = strlen(hd.bs)+1;
@@ -142,10 +154,12 @@ int syncml_obex_send_req () {
         if (OBEX_Request(handle, obj) < 0)
             printf("request");
 
-        printf("foo1\n");
-        while (OBEX_HandleInput(handle, 1) == 0);
-        printf("foo1a\n");
+        while (req_done < 1) {
+            OBEX_HandleInput(handle, 1);
+        }
     }
+
+    printf("CONNECTED\n");
 
     obj = OBEX_ObjectNew(handle, OBEX_CMD_GET);
     hd.bq4 = 0;
@@ -164,7 +178,34 @@ int syncml_obex_send_req () {
     
     if (OBEX_Request(handle, obj) < 0)
         printf("foo2\n");
-    while (OBEX_HandleInput(handle, 100) == 0);
+    while (req_done < 2) {
+        OBEX_HandleInput(handle, 1);
+    }
+
+    printf("LISTING DONE\n");
+
+    obj = OBEX_ObjectNew(handle, OBEX_CMD_GET);
+    hd.bq4 = 0;
+    size = 4;
+    OBEX_ObjectAddHeader(handle, obj, OBEX_HDR_CONNECTION, hd, size, OBEX_FL_FIT_ONE_PACKET);
+    
+    name="lspdata?name=0xbc61f02c87e32f9b&start_time=8782700699";
+    hd.bs = g_utf8_to_utf16(name, strlen(name), NULL, &num, NULL);
+
+    for (i=0; i<num; i++) {
+        uint16_t *wchar = &hd.bs[i*2];
+        *wchar = ntohs(*wchar);
+    }
+    size = (num+1) * sizeof(uint16_t);
+    OBEX_ObjectAddHeader(handle, obj, OBEX_HDR_NAME, hd, size, OBEX_FL_FIT_ONE_PACKET);
+    
+    if (OBEX_Request(handle, obj) < 0)
+        printf("foo2\n");
+    while (req_done < 3) {
+        OBEX_HandleInput(handle, 100);
+    }
+
+    printf("DATA DONE\n");
 
     obj = OBEX_ObjectNew(handle, OBEX_CMD_DISCONNECT);
     hd.bq4 = 0;
