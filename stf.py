fe16cdf053e52c014d7f86ff19681ba8a026227f
Steven Walter <stevenrwalter@gmail.com>
2010-01-27 22:22:49 -0500

    Connect the dots

--- stf.py
+++ stf.py
@@ -11,22 +11,22 @@ ctx.set_source_rgb(255, 255, 255)
 ctx.paint()
 ctx.set_source_rgb(0,0,0)
 
-def draw_point(x, y):
-    print "%d, %d" % (x, y)
-    ctx.move_to(x, y)
-    ctx.line_to(x+10, y)
-    ctx.line_to(x+10, y+10)
-    ctx.line_to(x, y+10)
-    ctx.close_path()
-    ctx.fill()
-
 class Parser(parsestf.STFParser):
+    def __init__(self, *args):
+        super(Parser, self).__init__(*args)
+        self.last_force=0
+
     def handle_point(self, x, y, f, time):
-        print time
-        draw_point(x, y)
+        if self.last_force:
+            ctx.line_to(x, y)
+        else:
+            ctx.stroke()
+            ctx.move_to(x, y)
+        self.last_force = f
 
 f = file(sys.argv[1])
 p = Parser(f)
 p.parse()
 
+ctx.stroke()
 surface.write_to_png(sys.argv[2])
